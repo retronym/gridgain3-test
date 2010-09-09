5a4d725fdf642818864d9d809e2f9c7651def868
retronym <jzaugg@gmail.com>
2010-09-09 07:31:45 +0200

    type annotations

--- src/main/scala/gridgaintest/PiSimApplication.scala
+++ src/main/scala/gridgaintest/PiSimApplication.scala
@@ -2,28 +2,31 @@ package gridgaintest
 
 import org.gridgain.grid.Grid
 import org.gridgain.scalar.scalar
-import scala.actors.Futures._
+import actors.Futures._
+import actors.Future
 
 object PiSimApplication {
   def main(args: Array[String]) {
     val MaxSimulations = 100000
     val SimsPerBlock = 1000
-    val RequiredVariance: Double = 0.0001
-    val NumWorkers: Int = 8
+    val RequiredVariance = 0.0001
+    val NumWorkers = 8
 
-    scalar {
+    for (i <- 1 to 100) scalar {
       (grid: Grid) =>
         val runners = Seq(
           new SimpleConvergingMonteCarloSimulationRunner(MaxSimulations, SimsPerBlock),
           new GridGainConvergingMonteCarloSimulationRunner(NumWorkers, MaxSimulations, SimsPerBlock, grid))
-        val sims = for{
+
+        val sims: Seq[Future[(String, Option[Double])]]= for{
           runner <- runners // Use different simulation runners
-          sim <- 1 to 2     // Run a few simulations in parallel to check thread safety of the StatisticsAggregatorActor
-        } yield {
-          future((runner.getClass.getSimpleName,
-                  runner(new Pi.Simulation(RequiredVariance))))
-        }
-        println(sims.map(_()).mkString("\n"))
+          sim <- 1 to 8     // Run a few simulations in parallel to check thread safety of the StatisticsAggregatorActor
+        } yield future((runner.getClass.getSimpleName, runner(new Pi.Simulation(RequiredVariance))))
+
+        // await the futures
+        val results: Seq[(String, Option[Double])] = sims.map(_())
+
+        println(results.mkString("\n"))
     }
   }
 }
