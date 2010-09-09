090804437bb7f8a941610ff19419d0e0808145dc
retronym <jzaugg@gmail.com>
2010-09-09 08:08:41 +0200

    attempt to create test case for shutdown hang.

---
+++ src/main/scala/gridgaintest/GridGainHangTestApplication.scala
@@ -0,0 +1,83 @@
+package gridgaintest
+
+import java.util.UUID
+import org.gridgain.grid.resources.GridTaskSessionResource
+import org.gridgain.grid._
+import org.gridgain.scalar.scalar
+
+/**
+ * An attempt to reproduce the hang on shutdown I see with PiSimApplication. Doesn't hang yet :(
+ */
+object GridGainHangTestApplication {
+  def main(args: Array[String]) {
+    for (i <- 0 until 10) scalar {
+      (grid: Grid) =>
+      def runTask = {
+        val master = grid.localNode
+        val task = new GridTaskNoReduceSplitAdapter[AnyRef] {
+          def split(gridSize: Int, t: AnyRef): java.util.Collection[_ <: GridJob] = {
+            val jobs: java.util.Collection[GridJob] = new java.util.ArrayList[GridJob]()
+            for (job <- (0 until 8))
+              jobs.add(new GridJob {
+                println("worker created")
+                @volatile var cancelled = false
+
+                def cancel = {cancelled = true}
+
+                @GridTaskSessionResource
+                def setSession(s: GridTaskSession) {session = s}
+
+                var session: GridTaskSession = _
+
+                def execute = {
+                  println("worker ready in session: " + session.getId)
+                  session.waitForAttribute("start")
+                  println("worker started in session: " + session.getId)
+                  def execute(i: Int): Unit = if (!cancelled && i <= 10) {
+                    println("sending message to master: %d".format(i))
+                    master.send(new Object(), null)
+                    try {
+                      TimeUnit.MILLISECONDS.sleep(100)
+                    } catch {
+                      case _: InterruptedException => // wake up
+                    }
+                    execute(i + 1)
+                  }
+                  execute(0)
+                  println("worker done")
+                  null
+                }
+              })
+            println("split into jobs: " + jobs)
+            jobs
+          }
+        }
+
+        val gridTaskFuture: GridTaskFuture[Void] = grid.execute(task, null)
+
+        master.listenAsync(new GridListenActor[AnyRef] {
+          var counter = 0
+
+          def receive(nodeId: UUID, recvMsg: AnyRef) = {
+            println("received: " + recvMsg)
+            counter += 1
+            if (counter > 20) {
+              println("cancelling task and deregistering actor")
+              gridTaskFuture.cancel
+              stop
+            }
+          }
+        })
+        gridTaskFuture.getTaskSession.setAttribute("start", "start")
+        println("waiting on gridTaskFuture")
+        try {
+          gridTaskFuture.get
+        } catch {
+          case _: GridFutureCancelledException => // ignore
+        }
+        println("gridTaskFuture completed")
+      }
+      Seq(future(runTask), future(runTask)).map(_())
+    }
+  }
+}
--- src/main/scala/gridgaintest/PiSimApplication.scala
+++ src/main/scala/gridgaintest/PiSimApplication.scala
@@ -1,9 +1,12 @@
 package gridgaintest
 
-import org.gridgain.grid.Grid
 import org.gridgain.scalar.scalar
 import actors.Futures._
 import actors.Future
+import java.util.concurrent.TimeUnit
+import org.gridgain.grid.resources.GridTaskSessionResource
+import org.gridgain.grid._
+import java.util.UUID
 
 object PiSimApplication {
   def main(args: Array[String]) {
@@ -11,16 +14,18 @@ object PiSimApplication {
     val SimsPerBlock = 1000
     val RequiredVariance = 0.0001
     val NumWorkers = 8
+    val GridRestarts: Int = 100
+    val ConcurrentSimulations = 1
 
-    for (i <- 1 to 100) scalar {
+    for (i <- 0 until GridRestarts) scalar {
       (grid: Grid) =>
         val runners = Seq(
           new SimpleConvergingMonteCarloSimulationRunner(MaxSimulations, SimsPerBlock),
           new GridGainConvergingMonteCarloSimulationRunner(NumWorkers, MaxSimulations, SimsPerBlock, grid))
 
-        val sims: Seq[Future[(String, Option[Double])]]= for{
+        val sims: Seq[Future[(String, Option[Double])]] = for{
           runner <- runners // Use different simulation runners
-          sim <- 1 to 8     // Run a few simulations in parallel to check thread safety of the StatisticsAggregatorActor
+          sim <- 0 until ConcurrentSimulations // Run a few simulations in parallel to check thread safety of the StatisticsAggregatorActor
         } yield future((runner.getClass.getSimpleName, runner(new Pi.Simulation(RequiredVariance))))
 
         // await the futures
